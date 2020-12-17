1aeb3d48347b209a19be2f639ff75f3e5f9f3bff
Ibra Ennara <ibraennara@gmail.com>
2020-12-17 11:47:14 -0500

    New Matrices setup
    world matrix, view matrix, and projection matrix all working, but not really used right now.

--- BlazorApp/Engine/Vertex.cs
+++ BlazorApp/Engine/Vertex.cs
@@ -7,6 +7,7 @@ using Blazor.Extensions.Canvas.WebGL;
 using Blazor.Extensions;
 using System.Numerics;
 using GlmNet;
+using Microsoft.JSInterop;
 
 
 namespace BlazorApp
@@ -16,13 +17,19 @@ namespace BlazorApp
         private WebGLContext _context;
         protected BECanvasComponent _canvasReference;
 
-        private const string VS_SOURCE = "attribute vec2 aPos;" +
+        [Inject]
+        internal IJSRuntime JSRuntime { get; set; }
+
+        private const string VS_SOURCE = "attribute vec3 aPos;" +
                                          "attribute vec3 aColor;" +
                                          "varying vec3 vColor;" +
+                                         "uniform mat4 mWorld;" +
+                                         "uniform mat4 mView;" +
+                                         "uniform mat4 mProj;" +
 
                                          "void main() {" +
                                             "vColor = aColor;" +
-                                            "gl_Position = vec4(aPos, 0.0, 1.0);" +
+                                            "gl_Position = mProj * mView * mWorld * vec4(aPos, 1.0);" +
                                          "}";
 
         private const string FS_SOURCE = "precision mediump float;" +
@@ -45,10 +52,10 @@ namespace BlazorApp
 
             var vertices = new[] // Using an index buffer to avoid overlap isnt working for some reason ...
             {
-                // X     Y          R    G    B
-                 0.0f,  0.5f,     1.0f, 1.0f, 0.0f,     // top right
-                 -0.5f, -0.5f,     0.7f, 0.0f, 1.0f,    // bottom right
-                 0.5f,  -0.5f,    0.1f, 1.0f, 0.6f      // top left 
+                // X     Y    Z           R    G    B
+                 0.0f,  0.5f, 0.0f,      1.0f, 1.0f, 0.0f,     // top right
+                 -0.5f, -0.5f, 0.0f,     0.7f, 0.0f, 1.0f,    // bottom right
+                 0.5f,  -0.5f, 0.0f,     0.1f, 1.0f, 0.6f      // top left 
             };
 
             // Everything below here is where drawing an object is done
@@ -59,15 +66,33 @@ namespace BlazorApp
             var colorAttribLocation = await _context.GetAttribLocationAsync(program, "aColor");
 
 
-            await this._context.VertexAttribPointerAsync((uint)positionAttribLocation, 2, DataType.FLOAT, false, 5 * sizeof(float), 0); // index, size of the vertex, type, normalized, how much info in a vertex
-            await this._context.VertexAttribPointerAsync((uint)colorAttribLocation, 3, DataType.FLOAT, false, 5 * sizeof(float), 2 * sizeof(float)); //offset here because the color is on the 4th value
+            await this._context.VertexAttribPointerAsync((uint)positionAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 0); // index, size of the vertex, type, normalized, how much info in a vertex
+            await this._context.VertexAttribPointerAsync((uint)colorAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 3 * sizeof(float)); //offset here because the color is on the 4th value
             await this._context.EnableVertexAttribArrayAsync((uint)positionAttribLocation);
             await this._context.EnableVertexAttribArrayAsync((uint)colorAttribLocation);
 
+            await this._context.UseProgramAsync(program); // Tell OpenGl which program should be active
+
+            var matWorldUniformLocation = await _context.GetUniformLocationAsync(program, "mWorld");
+            var matViewUniformLocation = await _context.GetUniformLocationAsync(program, "mView");
+            var matProjUniformLocation = await _context.GetUniformLocationAsync(program, "mProj");
+
+            mat4 worldMatrix = new mat4();
+            mat4 viewMatrix = new mat4();
+            mat4 projMatrix = new mat4();
+            // cant do identity here...
+
+            // Camera is setup here
+            worldMatrix = mat4.identity();
+            viewMatrix = glm.lookAt(new vec3(0, 0, -2), new vec3(0, 0, 0), new vec3(0, 1, 0));
+            projMatrix = glm.perspective(glm.radians(45), _canvasReference.Width / _canvasReference.Height, 0.1f, 1000.0f);
+
+            await _context.UniformMatrixAsync(matWorldUniformLocation, false, worldMatrix.to_array());
+            await _context.UniformMatrixAsync(matViewUniformLocation, false, viewMatrix.to_array());
+            await _context.UniformMatrixAsync(matProjUniformLocation, false, projMatrix.to_array());
+
 
             // Here is where we WOULD do a render loop, like while (true)
-
-            await this._context.UseProgramAsync(program);
 
             await this._context.DrawArraysAsync(Primitive.TRIANGLES, 0, 3); // type, how many to skip, how many vertices
         }
