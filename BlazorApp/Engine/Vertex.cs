38d70898bd4765cb7ffd66a3934f37e33ad0a8e8
Ibra Ennara <ibraennara@gmail.com>
2020-12-17 14:59:43 -0500

    ANIMATION!!
    Working spinning triangle, although its a bit flickery because of ClearAsync, and the triangle spins extremely fast... but it works

--- BlazorApp/Engine/Vertex.cs
+++ BlazorApp/Engine/Vertex.cs
@@ -16,6 +16,10 @@ namespace BlazorApp
     {
         private WebGLContext _context;
         protected BECanvasComponent _canvasReference;
+        float angle;
+        mat4 worldMatrix = new mat4();
+        mat4 viewMatrix = new mat4();
+        mat4 projMatrix = new mat4();
 
         [Inject]
         internal IJSRuntime JSRuntime { get; set; }
@@ -41,62 +45,79 @@ namespace BlazorApp
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            this._context = await this._canvasReference.CreateWebGLAsync();
-
-            await this._context.ClearColorAsync(1, 1, 1, 1);
-            await this._context.ClearAsync(BufferBits.COLOR_BUFFER_BIT | BufferBits.DEPTH_BUFFER_BIT);
-            var program = await this.InitProgramAsync(this._context, VS_SOURCE, FS_SOURCE);
-
-            var vertexBuffer = await this._context.CreateBufferAsync(); // Create a buffer to pass variables to the GPU RAM.
-            await this._context.BindBufferAsync(BufferType.ARRAY_BUFFER, vertexBuffer);
-
-            var vertices = new[] // Using an index buffer to avoid overlap isnt working for some reason ...
+            if (firstRender)
             {
+
+                this._context = await this._canvasReference.CreateWebGLAsync();
+
+                await this._context.ClearColorAsync(1, 1, 1, 1);
+                await this._context.ClearAsync(BufferBits.COLOR_BUFFER_BIT | BufferBits.DEPTH_BUFFER_BIT);
+                var program = await this.InitProgramAsync(this._context, VS_SOURCE, FS_SOURCE);
+
+                var vertexBuffer = await this._context.CreateBufferAsync(); // Create a buffer to pass variables to the GPU RAM.
+                await this._context.BindBufferAsync(BufferType.ARRAY_BUFFER, vertexBuffer);
+
+                var vertices = new[] // Using an index buffer to avoid overlap isnt working for some reason ...
+                {
                 // X     Y    Z           R    G    B
                  0.0f,  0.5f, 0.0f,      1.0f, 1.0f, 0.0f,     // top right
                  -0.5f, -0.5f, 0.0f,     0.7f, 0.0f, 1.0f,    // bottom right
                  0.5f,  -0.5f, 0.0f,     0.1f, 1.0f, 0.6f      // top left 
-            };
+                };
 
-            // Everything below here is where drawing an object is done
-            await this._context.BufferDataAsync(BufferType.ARRAY_BUFFER, vertices, BufferUsageHint.STATIC_DRAW); // Specifying which array to pass to the GPU 
-            //await this._context.BufferDataAsync(BufferType.ELEMENT_ARRAY_BUFFER, indices, BufferUsageHint.STATIC_DRAW); // index buffer
+                // Everything below here is where drawing an object is done
+                await this._context.BufferDataAsync(BufferType.ARRAY_BUFFER, vertices, BufferUsageHint.STATIC_DRAW); // Specifying which array to pass to the GPU 
+                                                                                                                     //await this._context.BufferDataAsync(BufferType.ELEMENT_ARRAY_BUFFER, indices, BufferUsageHint.STATIC_DRAW); // index buffer
 
-            var positionAttribLocation = await _context.GetAttribLocationAsync(program, "aPos");
-            var colorAttribLocation = await _context.GetAttribLocationAsync(program, "aColor");
-
-
-            await this._context.VertexAttribPointerAsync((uint)positionAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 0); // index, size of the vertex, type, normalized, how much info in a vertex
-            await this._context.VertexAttribPointerAsync((uint)colorAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 3 * sizeof(float)); //offset here because the color is on the 4th value
-            await this._context.EnableVertexAttribArrayAsync((uint)positionAttribLocation);
-            await this._context.EnableVertexAttribArrayAsync((uint)colorAttribLocation);
-
-            await this._context.UseProgramAsync(program); // Tell OpenGl which program should be active
-
-            var matWorldUniformLocation = await _context.GetUniformLocationAsync(program, "mWorld");
-            var matViewUniformLocation = await _context.GetUniformLocationAsync(program, "mView");
-            var matProjUniformLocation = await _context.GetUniformLocationAsync(program, "mProj");
-
-            mat4 worldMatrix = new mat4();
-            mat4 viewMatrix = new mat4();
-            mat4 projMatrix = new mat4();
-            // cant do identity here...
-
-            // Camera is setup here
-            worldMatrix = mat4.identity();
-            viewMatrix = glm.lookAt(new vec3(0, 0, -2), new vec3(0, 0, 0), new vec3(0, 1, 0));
-            projMatrix = glm.perspective(glm.radians(45), _canvasReference.Width / _canvasReference.Height, 0.1f, 1000.0f);
-
-            await _context.UniformMatrixAsync(matWorldUniformLocation, false, worldMatrix.to_array());
-            await _context.UniformMatrixAsync(matViewUniformLocation, false, viewMatrix.to_array());
-            await _context.UniformMatrixAsync(matProjUniformLocation, false, projMatrix.to_array());
+                var positionAttribLocation = await _context.GetAttribLocationAsync(program, "aPos");
+                var colorAttribLocation = await _context.GetAttribLocationAsync(program, "aColor");
 
 
-            // Here is where we WOULD do a render loop, like while (true)
+                await this._context.VertexAttribPointerAsync((uint)positionAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 0); // index, size of the vertex, type, normalized, how much info in a vertex
+                await this._context.VertexAttribPointerAsync((uint)colorAttribLocation, 3, DataType.FLOAT, false, 6 * sizeof(float), 3 * sizeof(float)); //offset here because the color is on the 4th value
+                await this._context.EnableVertexAttribArrayAsync((uint)positionAttribLocation);
+                await this._context.EnableVertexAttribArrayAsync((uint)colorAttribLocation);
 
-            await this._context.DrawArraysAsync(Primitive.TRIANGLES, 0, 3); // type, how many to skip, how many vertices
+                await this._context.UseProgramAsync(program); // Tell OpenGl which program should be active
+
+                var matWorldUniformLocation = await _context.GetUniformLocationAsync(program, "mWorld");
+                var matViewUniformLocation = await _context.GetUniformLocationAsync(program, "mView");
+                var matProjUniformLocation = await _context.GetUniformLocationAsync(program, "mProj");
+
+                var dotNetReference = DotNetObjectReference.Create(this);
+                await JSRuntime.InvokeVoidAsync("anim.start", dotNetReference, matWorldUniformLocation);
+
+                // Camera is setup here
+                worldMatrix = mat4.identity();
+                viewMatrix = glm.lookAt(new vec3(0, 0, -2), new vec3(0, 0, 0), new vec3(0, 1, 0));
+                projMatrix = glm.perspective(glm.radians(45), _canvasReference.Width / _canvasReference.Height, 0.1f, 1000.0f);
+
+                await _context.UniformMatrixAsync(matWorldUniformLocation, false, worldMatrix.to_array());
+                await _context.UniformMatrixAsync(matViewUniformLocation, false, viewMatrix.to_array());
+                await _context.UniformMatrixAsync(matProjUniformLocation, false, projMatrix.to_array());
+
+
+                // Here is where we WOULD do a render loop, like while (true)
+
+                //await this._context.DrawArraysAsync(Primitive.TRIANGLES, 0, 3); // type, how many to skip, how many vertices
+            }
         }
 
+        [JSInvokable("eventRequestAnimationFrame")]
+        public async Task eventRequestAnimationFrame(float time, WebGLUniformLocation location)
+        {
+            // MAIN RENDER LOOP
+            //Console.WriteLine("test");
+
+            angle = time / 1000 / 6 * 2 * MathF.PI; // not a good idea to create new variables here, this is a loop.
+            worldMatrix = glm.rotate(worldMatrix, angle, new vec3(0, 1, 0));
+
+            await _context.UniformMatrixAsync(location, false, worldMatrix.to_array());
+
+            await _context.ClearColorAsync(0.75f, 0.85f, 0.8f, 1.0f);
+            await this._context.ClearAsync(BufferBits.COLOR_BUFFER_BIT | BufferBits.DEPTH_BUFFER_BIT);
+            await this._context.DrawArraysAsync(Primitive.TRIANGLES, 0, 3); // type, how many to skip, how many vertices
+        }
         private async Task<WebGLProgram> InitProgramAsync(WebGLContext gl, string vsSource, string fsSource)
         {
             var vertexShader = await this.LoadShaderAsync(gl, ShaderType.VERTEX_SHADER, vsSource); // compiling shaders
